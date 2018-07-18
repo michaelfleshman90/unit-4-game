cd1002f09817987cb31a4c76d4d490cc4c2916e3
Michael Fleshman <michaelfleshman90@gmail..com>
2018-07-18 18:04:45 -0400

    image and game object

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+var game = {
+    
+}
+
+var obiWanKenobi = {
+    healthPoints: 150,
+    attackPower: Math.pow(3, 5),
+    counterAttackPower: 25,
+
+}
+var yoda = {
+    healthPoints: 160,
+    attackPower: Math.pow(4, 5),
+    counterAttackPower: 35,
+    
+}
+var darthMaul = {
+    healthPoints: 170,
+    attackPower: Math.pow(3, 6),
+    counterAttackPower: 30,
+    
+}
+var countDuku = {
+    healthPoints: 130,
+    attackPower: Math.pow(2, 3),
+    counterAttackPower: 15,
+    
+}

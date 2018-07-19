4e0b8f482a2cf9c5f057e439ef48c866e6ab1608
Michael Fleshman <michaelfleshman90@gmail..com>
2018-07-18 23:47:49 -0400

    loop and if statement add

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,62 @@
 var game = {
-    playerfighter: function() {
-        $("#characterChoice").click(function(){
-            $(this).appendTo(".attackarea");
+    characterSelected: "",
+    enemyFighter: "",
+    playerFighter: function() {
+        for (i=0; i < 4; i++) {
+            $("#character" + i).on("click", function() {
+                if (game.characterSelected == "") {
+                    game.characterSelected = $(this).attr("id").toString();
+                    $(this).appendTo("#attackArea");
+                    $(this).unBind("click");
+                    
+                } else if (game.enemyFighter == "") {
+                    game.enemyFighter = $(this).attr("id").toString();
+                    $(this).appendTo("#defenderArea");
+                    $(this).unBind("click");
+                }
+            });
+        }
+    },
+    attack: function() {
+        $("#attackButton").on("click", function() {
+            if(game.characterSelected != "" && game.enemyFighter != "") {
+                
+            }
     });
+
+    },
+    
+    obiWanKenobi: {
+        healthPoints: 150,
+        attackPower: Math.pow(3, 5),
+        counterAttackPower: 25,
+
+    },
+    yoda: {
+        healthPoints: 160,
+        attackPower: Math.pow(4, 5),
+        counterAttackPower: 35,
+        
+    },
+    darthMaul: {
+        healthPoints: 170,
+        attackPower: Math.pow(3, 6),
+        counterAttackPower: 30,
+        
+    },
+    countDuku: {
+        healthPoints: 130,
+        attackPower: Math.pow(2, 3),
+        counterAttackPower: 15,
+        
     }
 }
 
-var obiWanKenobi = {
-    healthPoints: 150,
-    attackPower: Math.pow(3, 5),
-    counterAttackPower: 25,
 
-}
-var yoda = {
-    healthPoints: 160,
-    attackPower: Math.pow(4, 5),
-    counterAttackPower: 35,
-    
-}
-var darthMaul = {
-    healthPoints: 170,
-    attackPower: Math.pow(3, 6),
-    counterAttackPower: 30,
-    
-}
-var countDuku = {
-    healthPoints: 130,
-    attackPower: Math.pow(2, 3),
-    counterAttackPower: 15,
-    
-}
-//player select character
 $(document).ready(function() {
-    game.playerfighter();
-    
+    game.playerFighter(); 
 });
+
+
+
+
